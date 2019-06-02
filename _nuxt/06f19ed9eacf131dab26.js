ece1ea450f9014587933dc2c53016356571f138a
ddio <ddioibb@gmail.com>
2019-06-02 15:21:56 +0800

    369063d

---
+++ _nuxt/06f19ed9eacf131dab26.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{443:function(n,o,e){"use strict";e.r(o);var t={fetch:function(n){(0,n.redirect)(307,"/blog")}},c=e(15),component=Object(c.a)(t,void 0,void 0,!1,null,null,null);o.default=component.exports}}]);
